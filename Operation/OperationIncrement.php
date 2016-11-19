e8ddad744e31e978e5d8d2a30ad2cf03ad54b6dd
Bruno Hanai <bruno.hanai@gmail.com>
2016-11-19 02:16:30 -0200

    Trata o $current_value da Sum e Inc Operation

--- Operation/OperationIncrement.php
+++ Operation/OperationIncrement.php
@@ -6,6 +6,8 @@ class OperationIncrement implements OperationInterface
 {
     public function doOperation($current_value, $new_value = null, array $context = array())
     {
+        $current_value = empty($current_value) || is_null($current_value) ? 0 : $current_value;
+
         if (!is_numeric($current_value)) {
             throw new \LogicException('The values must be numeric.');
         }
--- Operation/OperationSum.php
+++ Operation/OperationSum.php
@@ -6,6 +6,8 @@ class OperationSum implements OperationInterface
 {
     public function doOperation($current_value = null, $new_value = null, array $context = array())
     {
+        $current_value = is_null($current_value) || empty($current_value) ? 0 : $current_value;
+
         if (!is_numeric($current_value) || !is_numeric($new_value)) {
             throw new \LogicException('The values must be numeric.');
         }
